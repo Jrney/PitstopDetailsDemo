28ccd9710789cf6625a7f723664b1b118a1a3a2f
Chelsea Lura <mail@chelsealura.com>
2014-05-27 11:50:24 -0700

    Cleaned up files and removed unneeded code

--- app/js/app.js
+++ app/js/app.js
@@ -12,20 +12,6 @@ define([
     Isotope,
     PlacesView
     ) {
-    //Simple example Place object
-    function Place (name, address, rating, price) {
-        this.name = name;
-        this.address = address;
-        this.rating = rating;
-        this.price = price;
-    }
-
-    Place.prototype.sayInfo = function() {
-        return (this.name +", "+ this.address +", "+ this.rating +", "+ this.price);
-    };
-    var examplePlace = new Place('UPS','123 Fake St.', 3, '$$' );
-
-    console.log(examplePlace.sayInfo());
 
     //ISOTOPE
     var iso = new Isotope ("#container", {
